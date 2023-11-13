819e4b382f6caed4b711f4c1b87befa64a92c9e6
zixuanh <cloudsky.marcus@gmail.com>
2023-11-13 18:49:46 -0500

    combine msg def and src

---
+++ src/lightweight_vicon_bridge_node.cpp
@@ -0,0 +1,19 @@
+// #include <ros/ros.h>
+// #include <iostream>
+// #include <string>
+// #include <lightweight_vicon_bridge/MocapState.h>
+// #include <Client.h>
+
+#include <iostream>
+#include <string>
+#include <Client.h>
+#include "rclcpp/rclcpp.hpp"
+#include "lightweight_vicon_bridge/msg/mocap_state.hpp"
+
+
+
+int main(int argc, char** argv)
+{
+   printf("ahhah\n");
+    return 0;
+}
