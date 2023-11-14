7de41aa877a44694f954c3285a532c3a423eb5d6
zixuanh <cloudsky.marcus@gmail.com>
2023-11-13 20:39:42 -0500

    hacky version: runnable but requires setting LD_LIBRARY_PATH

--- src/lightweight_vicon_bridge_node.cpp
+++ src/lightweight_vicon_bridge_node.cpp
@@ -36,7 +36,7 @@ int main(int argc, char** argv)
     // std::string tracker_name;
     // std::string tracker_topic;
     // std::string stream_mode;
-    // bool use_sim_time = false;
+     bool use_sim_time = false;
     // nhp.param(std::string("tracker_hostname"), tracker_hostname, std::string("192.168.2.161"));
     // nhp.param(std::string("tracker_port"), tracker_port, std::string("801"));
     // nhp.param(std::string("tracker_frame_name"), tracker_frame_name, std::string("mocap_world"));
@@ -51,7 +51,7 @@ int main(int argc, char** argv)
     std::string tracker_name = node->declare_parameter<std::string>("tracker_name", "mocap");
     std::string tracker_topic = node->declare_parameter<std::string>("tracker_topic", "mocap_tracking");
     std::string stream_mode = node->declare_parameter<std::string>("stream_mode", "ServerPush");
-    bool use_sim_time = node->declare_parameter<bool>("use_sim_time", false);
+//    use_sim_time = node->declare_parameter<bool>("use_sim_time", false);
 
 
     if (use_sim_time)
